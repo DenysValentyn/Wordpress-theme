f52032f5466fbfc13f154879331b6898f5ba0b71
DenysValentyn <kalynivdenysvalentyn@gmail.com>
2024-07-30 22:00:11 -0500

    Update page.php
    Co-authored-by: fantasy1114 <fantasy971114@gmail.com>

--- page.php
+++ page.php
@@ -7,6 +7,7 @@
 get_header();
 ?>
 
+
 <main id="main" class="main-content" role="main">
     <div id="<?php echo esc_attr(get_the_ID());?>" class="row main-row">
         <div class="page-wrap content-wrap col-lg-9">
